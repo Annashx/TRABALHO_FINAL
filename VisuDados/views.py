55a52ba4d6def7cd9af2a0f642dc322da9cca8b4
luiza <luiza.raissa07@aluno.ifce.edu.br>
2023-06-06 16:32:30 -0300

    5

--- VisuDados/views.py
+++ VisuDados/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import *
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 # Create your views here.
+
 
 def index(request):
     dt = [] 
@@ -10,5 +13,11 @@ def index(request):
     for v in val:
         dt.append(v.dado)
         k.append(v.dado2)
-        la.append(int(v.label))
+        la.append(v.label)
     return render(request, 'index.html', {'dt2': dt, 'la2': la, 'k2': k})
+
+def achak(c, t):
+    reg = LinearRegression().fit(c.reshape(-1, 1), t)
+    return reg.coef_
+
+kcerto = 0.52
