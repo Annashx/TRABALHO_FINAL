41aa5cb4cb699994829f07edccbee6556246dcde
luiza <luiza.raissa07@aluno.ifce.edu.br>
2023-06-19 10:07:11 -0300

    Maisum

--- VisuDados/static/dashboard.js
+++ VisuDados/static/dashboard.js
@@ -8,19 +8,37 @@
   const ctx = document.getElementById('myChart')
   // eslint-disable-next-line no-unused-vars
   const myChart = new Chart(ctx, {
-    type: 'line',
     data: {
       labels: conc,
       datasets: [{
+        type: 'line',
         data: temp,
         lineTension: 0.3,
         backgroundColor: '#4201e4',
         borderColor: '#4203e4',
         borderWidth: 0,
-        pointBackgroundColor: '#4201e4'
+        pointBackgroundColor: '#4201e4',
+      }, {
+        type: 'line',
+        data: reg,
+        lineTension: 0.3,
+        backgroundColor: '#4201e4',
+        borderColor: '#4203e4',
+        borderWidth: 3,
+        pointBackgroundColor: '#4201e4',
       }]
     },
     options: {
+      scales: {
+        x: {
+          type: 'linear',
+          position: 'bottom',
+        },
+        y: {
+          type: 'linear',
+          position: 'bottom',
+        }
+      },
       plugins: {
         legend: {
           display: false,
--- VisuDados/views.py
+++ VisuDados/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render
 from .models import *
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+import numpy as np
+import matplotlib.pyplot as plt
 # Create your views here.
+def estimate_coef(x, y):
+	# number of observations/points
+	n = np.size(x)
 
+	# mean of x and y vector
+	m_x = np.mean(x)
+	m_y = np.mean(y)
+
+	# calculating cross-deviation and deviation about x
+	SS_xy = np.sum(y*x) - n*m_y*m_x
+	SS_xx = np.sum(x*x) - n*m_x*m_x
+
+	# calculating regression coefficients
+	b_1 = SS_xy / SS_xx
+	b_0 = m_y - b_1*m_x
+
+	return (b_0, b_1)
 
 def index(request):
     temp = [] 
     conc = []
-    ns = []
+    reg = []
     val = Data.objects.all()
+    coef = estimate_coef(np.array(conc), np.array(temp))
+    print(coef)
     for v in val:
         temp.append(v.temperatura)
         conc.append(v.concentracao)
-        ns.append(v.temperatura * v.concentracao)
-    return render(request, 'index.html', {'temp2': temp, 'conc2': conc, 'ns2': ns})
+        reg.append(0.52 * v.concentracao)
+        #reg.append(coef[0] * v.concentracao + coef[1])
+    return render(request, 'index.html', {'temp2': temp, 'conc2': conc, 'reg2': reg})
 
-def achak(c, t):
-    reg = LinearRegression().fit(c.reshape(-1, 1), t)
-    return reg.coef_
-
-kcerto = 0.52
